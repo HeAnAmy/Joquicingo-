d672874bd4e756ceda9e5bc0febb5b7966b5a9ed
amyhean <72956190+Amyhean@users.noreply.github.com>
2023-10-30 20:27:06 -0600

    git commit -m "Se suben cambios"
    Se arregla footer

---
+++ JS/footer.js
@@ -0,0 +1,35 @@
+let footer = document.getElementById("footer");
+footer.insertAdjacentHTML("beforebegin",
+        `<footer class="footer-clean">
+          <div class="container">
+              <div class="row justify-content-center">
+                  <div class="col-sm-3 col-md-2 item">
+                    <a href="mailto:info@parquejoquicingo.org" target="_blank"><img src="../SRC/img/sobre.png" alt="" width="35px" height="35px"></a>
+                    <a href="https://www.facebook.com/Joxicingo" target="_blank"><img src="../SRC/img/facebook.png" alt="" width="35px" height="35px"></a>
+                    <a href="https://www.instagram.com/" target="_blank"><img src="../SRC/img/instagram.png" alt="" width="35px" height="35px"></a>
+                  </div>
+                  <div class="col-sm-3 col-md-2 item">
+                      <h3>Invitanos un Cafe</h3>
+                      <ul>
+                        <li>
+                          <a href="">México</a>
+                        </li>
+                        <li>
+                          <a href="">Otros Paises</a>
+                        </li>
+                      </ul>
+                  </div>
+                  <div class="col-sm-3 col-md-2 item">
+                      <h3>Actividades</h3>
+                      <ul>
+                          <li><a href="" target="_blank">Proximas</a></li>
+                          <li><a href="">2022-2020</a></li>
+                      </ul>
+                  </div>
+                  <div class="col-lg-3 item social">
+                    <img src="./src/img/2.png" alt="logo" width="300px" height="200px">
+                  </div>
+                  <p class="copyright" >Parque de estudios y reflexión Joquicingo © 2023</p>
+              </div>
+          </div>
+      </footer>`);
